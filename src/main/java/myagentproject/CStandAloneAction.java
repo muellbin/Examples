52ced79b8a276cc111e5dc22f3e1e44af69c52d7
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-05-15 15:51:43 +0200

    update annotation

--- src/main/java/myagentproject/CStandAloneAction.java
+++ src/main/java/myagentproject/CStandAloneAction.java
@@ -56,7 +56,7 @@ final class CStandAloneAction extends IBaseAction
 
     @Override
     public final IFuzzyValue<Boolean> execute( final IContext p_context, final boolean p_parallel, final List<ITerm> p_argument,
-                                               final List<ITerm> p_return, final List<ITerm> p_annotation
+                                               final List<ITerm> p_return
     )
     {
         // output of the calling agent and get parameter with correct typing
