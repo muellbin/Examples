e83ded26151de10dc5a2d93f53148b41e1a2ac75
Ehsan Tatasadi <ehsan.tatasadi@gmail.com>
2017-07-24 15:08:48 +0200

    add documentation to simulation.js

--- src/main/webapp/org/lightjason/trafficsimulation/html/js/simulation.js
+++ src/main/webapp/org/lightjason/trafficsimulation/html/js/simulation.js
@@ -22,9 +22,18 @@
 
 
 jQuery(function() {
+
+    /** array of vehicles */
     var vehicles = {};
+
+    // fit simulation size to its container
     document.getElementById('simulation-screen').width = jQuery("#simulation-dashboard").width();
     document.getElementById('simulation-screen').height = jQuery("#simulation-dashboard").height();
+
+    /**
+     * Quintus object (Simulation UI)
+     * @see http://www.html5quintus.com/
+     */
     var l_quintus = Quintus()
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio")
         .setup("simulation-screen", {
@@ -35,6 +44,7 @@ jQuery(function() {
         .controls()
         .touch();
 
+    // User vehicle sprite
     l_quintus.Sprite.extend("UserVehicle", {
         init: function (p) {
             this._super(p, {
@@ -48,6 +58,7 @@ jQuery(function() {
         }
     });
 
+    // default vehicle sprite
     l_quintus.Sprite.extend("DefaultVehicle", {
         init: function (p) {
             this._super(p, {
@@ -61,15 +72,23 @@ jQuery(function() {
         }
     });
 
+    // define two stages for street and vehicles
     l_quintus.stageScene("street", 0);
     l_quintus.stageScene("vehicles", 1);
 
+    // loading quintus elements
     l_quintus.load("axelf.mp3, sprites.png, sprites.json, streettiles.png", function () {
         l_quintus.sheet("streettiles", "streettiles.png", {tilew: 32, tileh: 32});
         l_quintus.compileSheets("sprites.png", "sprites.json");
         l_quintus.stageScene("street");
     });
 
+    /**
+     * generate the matrix of street tiles
+     * @param lanes the number of lanes
+     * @param length the length of the street in cells
+     * @return the matrix of the street tiles
+     */
     var streettiles = function( lanes, length )
     {
         var l_matrix = [];
@@ -88,6 +107,7 @@ jQuery(function() {
         return l_matrix;
     }
 
+    /** main functions of different elements */
     var objects =
     {
         vehicle:
@@ -96,13 +116,17 @@ jQuery(function() {
             {
                 if( p_data.userdefinied )
                 {
+                    // create the user vehicle
                     var l_uservehicle = vehicles[p_data.id] = new l_quintus.UserVehicle( {x: p_data.x * 32, y: p_data.y * 32 + 16} );
                     l_quintus.stages[1].insert( l_uservehicle );
+
+                    // the camera follows the user vehicle
                     l_quintus.stages[0].add("viewport").follow( l_uservehicle );
                     l_quintus.stages[1].add("viewport").follow( l_uservehicle );
                 }
                 else
                 {
+                    // create a default vehicle
                     var l_defaultvehicle = vehicles[p_data.id] = new l_quintus.DefaultVehicle( {x: p_data.x * 32, y: p_data.y * 32 + 16} );
                     //TODO: opposite vehicle
                     //if(opposite vehicle) l_defaultvehicle.p.angel = 180;
@@ -111,8 +135,7 @@ jQuery(function() {
             },
             execute: function( p_data )
             {
-                //console.log( p_data );
-
+                // move the vehicles in the new positions
                 vehicles[p_data.id].p.x = p_data.x * 32;
                 vehicles[p_data.id].p.y = p_data.y * 32 + 16;
             },
@@ -126,6 +149,9 @@ jQuery(function() {
             {
                 var lanes = p_data.lanes;
                 var length = p_data.length;
+
+                // a tilelayer in quintus can have the maximum size of 32768. (1024 cell * 32 pixcel)
+                // therefore we define multi tilelayers and put them together.
                 var maxcellinlayer = 1000;
                 var layercount = length / maxcellinlayer;
                 for (var i = 0; i < layercount; i++) {
@@ -154,10 +180,12 @@ jQuery(function() {
             remove: function( p_data )
             {
                 l_quintus.audio.stop();
+                //ToDo: after shut down, all vehicles should be deleted
             }
         }
     }
 
+    /** define the animation websocket */
     var ws = LightJason.websocket( "/animation" );
 
     ws.onopen = function()
