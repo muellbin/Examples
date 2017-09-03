b8bd5faa7c7b98dd7ac77989e11ba638d128c0c5
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-09-03 15:06:47 +0200

    fix style

--- src/main/java/org/lightjason/trafficsimulation/elements/vehicle/CVehicle.java
+++ src/main/java/org/lightjason/trafficsimulation/elements/vehicle/CVehicle.java
@@ -69,7 +69,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
